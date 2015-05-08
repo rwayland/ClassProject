194dd3b602f10b141a2afed44749f0d764c8ec49
derek <dseroky1@jhu.edu>
2015-05-08 15:10:03 -0400

    Changed roll Dice to static method rather than class method in Dice.py. Also, added a GamePiece class.

--- GameEngine/Dice.py
+++ GameEngine/Dice.py
@@ -3,10 +3,14 @@ __author__ = 'derek'
 import random
 
 
-class Dice:
+class Dice(object):
+    """
+        Simulates rolling of the dice.
+        @:return tuple containing the result of the roll.
+    """
 
-    @classmethod
-    def rollDice(cls):
+    @staticmethod
+    def rollDice():
         roll1 = random.randint(1, 6)
         roll2 = random.randint(1, 6)
         return roll1, roll2
