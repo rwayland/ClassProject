d0b4bdb21d4577bf7d31bd4c5f1bdb4314fc0484
derek <dseroky1@jhu.edu>
2015-05-12 17:33:25 -0400

    General logic updates to solve issues found while testing. Execute question phase is still in the works.

--- GameEngine/Player.py
+++ GameEngine/Player.py
@@ -7,19 +7,34 @@ from GamePiece import GamePiece
 class Player(object):
 
     _teamName = None
-    _currentPosition = None
-    _currentRollTotal = None
+    # _currentPosition = None
+    _currentRollTotal = 0
     _possibleWinCondition = False
     _gamePiece = None
+    _inSpoke = False
 
     def __init__(self):
         self._gamePiece = GamePiece()
+        self._currentPosition = 0
         return
 
     def getRollResult(self):
         result = Dice.rollDice()
-        self._currentRollTotal = result(0) + result(1)
+        self._currentRollTotal = int(result[0]) + int(result[1])
+        return self._currentRollTotal
 
     def finalizeTurn(self):
-        self._currentRollTotal = None
-        self._currentPosition = None
+        self._currentRollTotal = 0
+
+    def getCurrentPosition(self):
+        return self._currentPosition
+
+    def inSpoke(self):
+        return self._inSpoke
+
+    def setCurrentPosition(self, position, inSpoke = False):
+        self._currentPosition = position
+        self._inSpoke = inSpoke
+
+    def receiveWedge(self, color):
+        self._gamePiece.getWedge(color)
--- GameEngine/__init__.py
+++ GameEngine/__init__.py
@@ -3,6 +3,8 @@ __author__ = 'derek'
 from Dice import Dice
 from GamePiece import GamePiece
 from Player import Player
+from GameBoard import GameBoard
+from GameController import GameController
 
 # print(Dice.rollDice())
 # gamePiece = GamePiece()
@@ -10,5 +12,10 @@ from Player import Player
 # gamePiece.getWedge("blue")
 # gamePiece.getWedge("red")
 
-p1 = Player()
-print(p1.getRollResult())
+# p1 = Player()
+# print(p1.getRollResult())
+# gb = GameBoard()
+# myList = gb._buildGameBoard()
+# print myList[0:]
+# print(myList.__len__())
+GC = GameController()
